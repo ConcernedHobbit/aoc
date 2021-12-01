72d085a84aafd8983783f7dee84cc6bafe43a4d0
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-12-01 07:28:16 +0200

    day 1

---
+++ day1.py
@@ -0,0 +1,14 @@
+numbers = [int(n) for n in open('day1.txt')]
+part_one = 0
+part_two = 0
+
+for i in range(len(numbers)):
+    if i > 0 and numbers[i] > numbers[i - 1]:
+        part_one += 1
+
+    if i > 2 and sum(numbers[i - 3 : i]) > sum(numbers[i - 4 : i - 1]):
+        part_two += 1
+
+
+print(f'Part one: {part_one}')
+print(f'Part two: {part_two}')
