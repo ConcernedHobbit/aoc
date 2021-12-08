afbb4502c6a1e323a428e8bb9146a49ac5c5d441
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-12-08 21:34:06 +0200

    refactored day6

--- day6.py
+++ day6.py
@@ -1,26 +1,14 @@
 lanternfish = []
 for line in open('day6.txt'):
     lanternfish = [int(n) for n in line.strip().split(',')]
-original = lanternfish[:]
+in_state = [lanternfish.count(i) for i in range(9)]
 
-days = 80
-for day in range(days):
-    new_lanternfish = []
-    for fish in lanternfish:
-        if fish == 0:
-            new_lanternfish.append(6)
-            new_lanternfish.append(8)
-        else:
-            new_lanternfish.append(fish - 1)
+for day in range(256):
+    if day == 80:
+        print(f'Part 1: {sum(in_state)}')
 
-    lanternfish = new_lanternfish
-print(f'Part 1: {len(lanternfish)}')
-
-in_state = [original.count(i) for i in range(9)]
-days = 256
-for day in range(days):
     hatching = in_state.pop(0)
     in_state[6] += hatching
     in_state.append(hatching)
+
 print(f'Part 2: {sum(in_state)}')
-    
